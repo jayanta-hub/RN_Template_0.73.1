d0921d06520c1166ee440a6ad338fc88f0f1640e
Jayanta garu <76052991+jayanta-hub@users.noreply.github.com>
2023-12-25 17:28:07 +0530

    Update template.config.js

--- template.config.js
+++ template.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // Placeholder used to rename and replace in files
   // package.json, index.json, android/, ios/
-  placeholderName: "ProjectName",
+  placeholderName: "projectName",
 
   // Directory with template
   templateDir: "./template",
